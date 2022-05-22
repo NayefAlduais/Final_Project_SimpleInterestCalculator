8fadb916aa8c6b14378b16fe271aa8fad14ad42c
NayefAlduais <naifalduais@gmail.com>
2022-05-22 23:28:20 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function compute()
+{
+   
+    var principal  = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+
+    if(principal <= 0 ){
+        alert("Enter a positive number");
+        principal.focus();
+        return false;
+  } 
+  else
+  alert("OK!");
+{ 
+    document.getElementById("result").innerHTML="If you deposit " + principal + "<br/>" + "at an interest rate of" + rate+ "%." + "<br/>" + "You will receive an amount of"+ interest  + "<br/>" +"in the year " + year + "<br/>";
+    //document.getElementById("result").innerHTML= "If you deposit " + principal + "\n" +"at an interest rate of" + rate+ "%." ;
+}
+//document.getElementById("result").innerHTML="If you deposit " + principal ;
+
+}  
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
+        
